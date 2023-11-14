275d7f8ac637ce186e1ec8ad9dfc7336dc7fc8d8
helenahagman <helena@rapibo.se>
2023-11-14 13:53:04 +0000

    updated html templates and rendering

--- run.py
+++ run.py
@@ -30,6 +30,16 @@ def contact():
     return render_template("contact.html")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
